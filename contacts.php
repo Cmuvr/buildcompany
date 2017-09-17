5cffab8105e56c0007f9076dea8a679846763421
Cmuvr <Cmuvr@mail.ru>
2017-09-17 22:14:09 +0400

    first commit

---
+++ contacts.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="Style/style.css">
+    <title>Главная страница</title>
+</head>
+<body>
+    <div class="wrapper">
+       
+       
+       
+       
+       
+       
+       <header class="main_header">
+            <a href="#">
+                <img src="imgs/main_header.jpg" alt="">
+            </a>
+            <nav class="main_nav">
+                <ol>
+                    <li>
+                        <a href="index.php">Главная</a>                    
+                    </li>
+                    <li>
+                        <a href="news.php">Новости</a>
+                    </li>
+                    <li>
+                        <a href="our_work.php">Наша работы</a>                    
+                    </li>
+                    <li>
+                        <a href="about_us.php">О нас</a>
+                    </li>
+                    <li>
+                        <a href="contacts.php">Контакты</a>
+                    </li>
+                </ol>
+            </nav>
+        </header>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        <aside class="main_news">
+            <header>
+               <h1 class="main_news_header">Свежие новости</h1>
+            </header>
+            <section>
+                <article class="news_block">
+                    <h1>Наши новые поступления</h1>
+                    <p>
+                        На  склад поступили новые материалы для утепления домов
+                        <a href="news.php#news_1">Читать далее</a>
+                    </p>
+                </article>
+
+                <article class="news_block">
+                    <h1>Теперь доступны новые услуги</h1>
+                    <p>
+                        Наша компания теперь осуществляет разработку проектов домов разной сложности
+                        <a href="news.php#news_2">Читать далее</a>
+                    </p>
+                </article>
+
+                <article class="news_block">
+                    <h1>У нас можно арендовать строительную технику</h1>
+                    <p>
+                        У нас можно арендовать строительную технику для рытья катлованов и транщей любой сложности. А так  же
+                        <a href="news.php#news_3">Читать далее</a>
+                    </p>
+                </article>
+            </section>
+        </aside>
+        
+        
+        
+        
+        
+        
+        
+        <main class="main_content">
+            
+            
+             <article class="main_content_block">
+               
+                <div class="mcb_img_about_us">
+                    <img src="imgs/contacts.png" alt="">  
+                </div>
+
+                <h1 class="mcb_head" >
+                    контакты          
+                </h1>
+
+
+
+                <p class="mcb_article">
+                
+                    Акционерное общество «СтройТрансНефтеГаз» (АО «СТНГ»)Ленинградский пр., д. 39, стр. 80,  БЦ «SkyLight», г. Москва,Российская Федерация, 125167Тел.: +7 495 741-48-17
+Факс: +7 495 741-48-18
+E-mail: info@stg.ru
+                </p>
+            </article>
+            
+            
+        </main>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        <footer class="main_footer">
+            <nav class="main_nav_footer">
+                <ol>
+                    <li>
+                        <a href="#">Главная</a>                    
+                    </li>
+                    <li>
+                        <a href="#">Новости</a>
+                    </li>
+                    <li>
+                        <a href="#">Наша продукция</a>                    
+                    </li>
+                    <li>
+                        <a href="#">О нас</a>
+                    </li>
+                    <li>
+                        <a href="#">Контакты</a>
+                    </li>
+                </ol>
+            </nav>
+        </footer>
+        
+        
+        
+        
+        
+        
+    </div>
+</body>
+</html>
